32fff64369e6fc643cf1ed9a195ae095f5d21c9b
Jim Burbridge <jhechtf@gmail.com>
2023-12-29 23:08:30 -0800

    wip

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,9 +1,14 @@
+import { db } from '$lib/server/db/client.js';
+import { recipes } from '$lib/server/db/schema/recipes.table.js';
 import { redirect } from '@sveltejs/kit';
+import { asc } from 'drizzle-orm';
 
 export const load = async ({ locals }) => {
   const session = await locals.auth.validate();
   if(!session) redirect(302, '/login');
+
   return {
     session,
+    recipes: db.select().from(recipes).orderBy(asc(recipes.title)),
   };
 };
