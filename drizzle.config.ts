decaf73903765a3ecc9c8f3d480d50c4a7fc2637
Jim Burbridge <jhechtf@gmail.com>
2024-01-14 01:23:49 -0800

    seeing if this fixes the builds

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -10,6 +10,6 @@ export default {
   driver: 'pg',
   schemaFilter: ['auth', 'public'],
   dbCredentials: {
-    connectionString: process.env.DB_URL!,
+    connectionString: `${process.env.DB_URL}require`,
   },
 } satisfies Config;
