c731b5b59dddd483381b79c48c4888c11abfe6fb
Jim Burbridge <jhechtf@gmail.com>
2024-03-25 18:48:28 -0700

    ci: fixing vercel builds (hopefully)

--- src/lib/server/db/client.ts
+++ src/lib/server/db/client.ts
@@ -1,10 +1,11 @@
 import Pg from 'pg';
+import { dev } from '$app/environment';
 import { DB_URL } from '$env/static/private';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from '$lib/server/db/schema';
 
 export const pool = new Pg.Pool({
-	connectionString: `${DB_URL}`
+	connectionString: `${DB_URL}${dev ? '' : '?sslmode=require'}`
 });
 
 await pool.connect();
